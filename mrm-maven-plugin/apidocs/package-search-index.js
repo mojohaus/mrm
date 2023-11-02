516072ef14356cb804d391c242976400262873ca
Slawomir Jaranowski <s.jaranowski@gmail.com>
2023-11-02 23:58:14 +0100

    Site checkin for project Mock Repository Manager :: Project

---
+++ mrm-maven-plugin/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.mrm_maven_plugin"},{"l":"org.codehaus.mojo.mrm.maven"},{"l":"org.codehaus.mojo.mrm.plugin"}];updateSearchResults();
